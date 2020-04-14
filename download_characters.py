2852c16d77a8e5824cdb33b9ad7c86ae71023e32
Agisilaos Kounelis <kounelisagis@gmail.com>
2020-04-14 19:27:39 +0300

    fixed movie dublicates and no characters

--- download_characters.py
+++ download_characters.py
@@ -7,13 +7,14 @@ from tmdbv3api import TMDb, Movie
 from multiprocessing.dummy import Pool
 from multiprocessing import cpu_count
 import pandas as pd
+import config
 
 
 tmdb = TMDb()
-tmdb.api_key = 'API-KEY'
+tmdb.api_key = config.tmdb_key
 movie = Movie()
 
-CSV_IN = "data\\data.csv"
+CSV_IN = "data\\data1.csv"
 CSV_OUT = "data\\data2.csv"
 
 
@@ -27,21 +28,16 @@ def process_task(row):
             temp_movieName = re.sub(r'\(.+?\)\s*', '', movieName)
             movies_list = movie.search(temp_movieName)
 
-        movies_list.sort(key=lambda x: x.popularity, reverse=True)
-
         tmdbId = movies_list[0].id
 
         cast_list = (movie.credits(tmdbId)).cast
+        cast_list = [character for character in cast_list if character['character'] != ""]
+
+        if len(cast_list) == 0:
+            return None
 
         topCharacters = min(10, len(cast_list))
 
-        # write_file = open(CHARACTERS_DIR + "\\" + filename, "w", encoding="utf-8")
-
-        # for actor in cast_list[:topCharacters]:
-        #     if actor:
-        #         write_file.write(actor["character"]+"\n")
-        
-        # write_file.close() 
         return (movieName, filename, tmdbId, [actor["character"] for actor in cast_list[:topCharacters]])
 
     except Exception as e:
@@ -52,7 +48,7 @@ def process_task(row):
 
 if __name__ == "__main__":
 
-    df_in = pd.read_csv(CSV_IN)
+    df_in = pd.read_csv(CSV_IN, encoding='utf-8')
     data = list(df_in.itertuples(index=False))
 
     with Pool(10) as p:
@@ -61,6 +57,7 @@ if __name__ == "__main__":
     results = [x[:-1] + (";".join(x[-1]),) for x in results if x is not None]
 
     df_out = pd.DataFrame(results, columns = ['movieName', 'fileΝame', 'tmdbId', 'characters'])
+    df_out = df_out.drop_duplicates(subset=['tmdbId'], keep='first')
 
     df_out.to_csv(CSV_OUT, encoding='utf-8', index=False)
 
